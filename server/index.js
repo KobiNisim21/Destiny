954e39cefae0b56e4dd4676b62dafa387a902552
Kobin <kobinisim21@gmail.com>
2026-01-23 15:33:00 +0200

    Increase Express body parser limit to 50mb

--- server/index.js
+++ server/index.js
@@ -31,7 +31,8 @@ const app = express();
 const PORT = process.env.PORT || 5000; // Trigger Restart 2
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
